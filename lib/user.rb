db67359d7798b50d9bdbf60b18fabe77b893ef20
T-erry <terrywachuka10@gmail.com>
2023-02-14 21:07:37 +0300

    completed assignment

---
+++ lib/user.rb
@@ -0,0 +1,10 @@
+require 'pry'
+class User
+
+    attr_accessor :first_name, :last_name
+    def initiallize first, last
+        @first_name =first
+        @last_name =last
+    end
+end
+# binding.pry
